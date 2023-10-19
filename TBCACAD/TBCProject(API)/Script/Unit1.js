7d5a8311939f2c0c64d059a11e1a0f721cb28ab7
Skhila <97901103+Skhila@users.noreply.github.com>
2023-10-19 21:03:27 +0400

    Create Unit1.js

---
+++ TBCACAD/TBCProject(API)/Script/Unit1.js
@@ -0,0 +1,22 @@
+﻿function Test1()
+{
+  let orders = Aliases.Orders;
+  let mainForm = orders.MainForm;
+  let toolBar = mainForm.ToolBar;
+  toolBar.ClickItem(4, false);
+  let orderForm = orders.OrderForm;
+  let groupBox = orderForm.Group;
+  let textBox = groupBox.Customer;
+  textBox.Click(47, 9);
+  textBox.SetText("123123");
+  let button = orderForm.ButtonOK;
+  button.ClickButton();
+  let listView = mainForm.OrdersView;
+  listView.Click(131, 54);
+  listView.ClickItem("123123", "MyMoney");
+  toolBar.ClickItem(5, false);
+  textBox = groupBox.Street;
+  textBox.Click(78, 12);
+  textBox.SetText("123");
+  button.ClickButton();
+}
